bc340b0cc52114cdd9d375cc8457e227e793ad84
Joost Grunwald <joostgrunwald2001@gmail.com>
2022-02-10 21:03:41 +0100

    implemented split as well

--- MetRobert_DataCreation/train_test_split.py
+++ MetRobert_DataCreation/train_test_split.py
@@ -1,9 +1,28 @@
 import random
 
+sentences = 38654
+splitpoint = 33000
+
 with open('train.tsv','r') as source:
     data = [ (random.random(), line) for line in source ]
 data.sort()
+source.close()
 
-with open('output.tsv','w') as target:
+with open('output.tsv','w') as toshuffle:
     for _, line in data:
-        target.write( line )
+        toshuffle.write( line )
+toshuffle.close()
+
+currentsentence = 0
+
+train_split = open("train_split.tsv", "w")
+test_split = open("test_split.tsv", "w")
+
+with open('output.tsv', 'r') as tosplit:
+    for lineno, line in enumerate(tosplit):
+        if lineno >= splitpoint:
+            test_split.write(line)
+        else:
+            train_split.write(line)
+            
+        
