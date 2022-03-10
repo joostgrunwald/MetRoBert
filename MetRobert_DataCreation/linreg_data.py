68eced11513f17cb25ccf1e12a04143d6aecc125
Joost Grunwald <joostgrunwald2001@gmail.com>
2022-03-10 17:42:48 +0100

    implemented logaritmic regression

--- MetRobert_DataCreation/linreg_data.py
+++ MetRobert_DataCreation/linreg_data.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import PolynomialFeatures
+import math
 
 # we are a supermarket
 # we are going to save how many products are sold each day
@@ -30,13 +30,51 @@ print("f1 model score = " + str(r_sq_f1))
 
 # predict using our model 
 y_pred_110 = acc_model.predict(np.array([110]).reshape(1, -1))
-y_pred_120 = acc_model.predict(np.array([110]).reshape(1, -1))
-y_pred_130 = acc_model.predict(np.array([110]).reshape(1, -1))
-y_pred_140 = acc_model.predict(np.array([110]).reshape(1, -1))
-y_pred_150 = acc_model.predict(np.array([110]).reshape(1, -1))
+y_pred_120 = acc_model.predict(np.array([120]).reshape(1, -1))
+y_pred_130 = acc_model.predict(np.array([130]).reshape(1, -1))
+y_pred_140 = acc_model.predict(np.array([140]).reshape(1, -1))
+y_pred_150 = acc_model.predict(np.array([150]).reshape(1, -1))
 
 print(y_pred_110)
 print(y_pred_120)
 print(y_pred_130)
 print(y_pred_140)
 print(y_pred_150)
+
+# predict using logaritmic model
+acc_pred_110 = 83.0222 + 2.1976 * np.log(110)
+acc_pred_120 = 83.0222 + 2.1976 * np.log(120)
+acc_pred_130 = 83.0222 + 2.1976 * np.log(130)
+acc_pred_140 = 83.0222 + 2.1976 * np.log(140)
+acc_pred_150 = 83.0222 + 2.1976 * np.log(150)
+
+print(acc_pred_110)
+print(acc_pred_120)
+print(acc_pred_130)
+print(acc_pred_140)
+print(acc_pred_150)
+
+y_pred_110 = f1_model.predict(np.array([110]).reshape(1, -1))
+y_pred_120 = f1_model.predict(np.array([120]).reshape(1, -1))
+y_pred_130 = f1_model.predict(np.array([130]).reshape(1, -1))
+y_pred_140 = f1_model.predict(np.array([140]).reshape(1, -1))
+y_pred_150 = f1_model.predict(np.array([150]).reshape(1, -1))
+
+print(y_pred_110)
+print(y_pred_120)
+print(y_pred_130)
+print(y_pred_140)
+print(y_pred_150)
+
+# predict using logaritmic model
+f1_pred2_110 = 45.2291 + 7.1536 * np.log(110)
+f1_pred2_120 = 45.2291 + 7.1536 * np.log(120)
+f1_pred2_130 = 45.2291 + 7.1536 * np.log(130)
+f1_pred2_140 = 45.2291 + 7.1536 * np.log(140)
+f1_pred2_150 = 45.2291 + 7.1536 * np.log(150)
+
+print(f1_pred2_110)
+print(f1_pred2_120)
+print(f1_pred2_130)
+print(f1_pred2_140)
+print(f1_pred2_150)
