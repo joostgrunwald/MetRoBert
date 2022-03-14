1c6547a41f643be41a636cf5804d4c7afe02fdc7
Joost Grunwald <joostgrunwald2001@gmail.com>
2022-03-14 17:33:51 +0100

    updated bagging script for multiple sources

--- MetRobert_model/scripts/run_bagging.sh
+++ MetRobert_model/scripts/run_bagging.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-INDEXES=$(seq 0 9)
+INDEXES=$(seq 1 14)
 for i in $INDEXES
 do
-    echo "Running bagging for index $i"
-    nice -19 python3 main_dutch.py --bagging_index $i
+    INDEXES2=$(seq 0 $i)
+    for j in $INDEXES2
+    do
+        echo "Running bagging for index $i"
+        nice -19 python3 main_dutch.py --bagging_index $j --num_bagging $i
+    done
 done 
