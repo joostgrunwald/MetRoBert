a264d9ebfcd2e017b944bbfd947ef4a7c02cb364
Joost Grunwald <joostgrunwald2001@gmail.com>
2022-03-18 00:03:18 +0100

    added todos for further implementation

--- MetRobert_model/outputgenerator.py
+++ MetRobert_model/outputgenerator.py
@@ -24,3 +24,11 @@ with open('dev.tsv') as devfile, open('output.tsv', 'w') as outputfile:
     for line in devfile:
         if not any(bad_sen in line for bad_sen in badsentencelist):
             outputfile.write(line)
+
+#TODO: we now fixed the problem with wrong_devs
+#TODO: we also have to fix the problems with the other part of wrong_devs sentences
+#THAT IS: Using the wrong_devs index and the above loop, remove any part of the sentence predictions
+#THIS should result in a prediction table and a output.tsv of equal size
+
+#TODO using the above combine output.tsv and the two prediction sets
+#TODO add the current word calculated based upon the index and sentence
