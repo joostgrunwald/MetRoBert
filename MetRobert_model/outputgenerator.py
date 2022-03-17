0940cb0bbde6162bd17dc0ad133e29a8ccc0bd70
Joost Grunwald <joostgrunwald2001@gmail.com>
2022-03-17 23:56:49 +0100

    succesfully detect bad sentences

--- MetRobert_model/outputgenerator.py
+++ MetRobert_model/outputgenerator.py
@@ -13,9 +13,15 @@ with open (os.path.join(__location__, 'wrong_devs.txt')) as misFile:
         #extract sentence out of wrong devs
         sentence_start = line.find(",")
         sentence_end = line.rfind(",")
-        sentence = line[sentence_start+2:sentence_end])
+        sentence = line[sentence_start+2:sentence_end]
 
         #we generate a list of bad sentences
-        badsentencelist.append(sentence)
+        badsentencelist.append(sentence[:len(sentence)-1])
 
-
+    
+#Remove badsentences from dev.tsv
+with open('dev.tsv') as devfile, open('output.tsv', 'w') as outputfile:
+    for line in devfile:
+        for badsentence in badsentencelist:
+            if line.find(badsentence) != -1:
+                print(line)
