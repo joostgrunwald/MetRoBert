9bf3020cccec74ca5a394a5977265700232a8b1a
Joost Grunwald <joostgrunwald2001@gmail.com>
2022-03-17 23:45:22 +0100

    generate list of bad sentences

--- MetRobert_model/outputgenerator.py
+++ MetRobert_model/outputgenerator.py
@@ -4,7 +4,18 @@ import os
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
+badsentencelist = []
+
 #Generate list of badwords
 with open (os.path.join(__location__, 'wrong_devs.txt')) as misFile:
-    for line in misfile:
-        print(line)
+    for line in misFile:
+
+        #extract sentence out of wrong devs
+        sentence_start = line.find(",")
+        sentence_end = line.rfind(",")
+        sentence = line[sentence_start+2:sentence_end])
+
+        #we generate a list of bad sentences
+        badsentencelist.append(sentence)
+
+
