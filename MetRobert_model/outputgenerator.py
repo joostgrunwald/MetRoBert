3fbb949e21957b5cb5af8b0818fc8596e3d1ddcf
Joost Grunwald <joostgrunwald2001@gmail.com>
2022-03-21 15:05:35 +0100

    extensively upgraded towards csv file generation

--- MetRobert_model/outputgenerator.py
+++ MetRobert_model/outputgenerator.py
@@ -17,35 +17,62 @@ with open (os.path.join(__location__, 'wrong_devs.txt')) as misFile:
         sentence = line[sentence_start+2:sentence_end]
 
         #we generate a list of bad sentences
-        badsentencelist.append(sentence[:len(sentence)-1])
+        badsentencelist.append(sentence[:-1])
 
         #we generate a list of bad indexes
         badindexlist.append(line[:sentence_start-1])
-    
+
 #Remove badsentences from dev.tsv
-with open('dev.tsv') as devfile, open('output.tsv', 'w') as outputfile:
+with open('dev.tsv') as devfile, open('dev2.tsv', 'w') as outputfile:
     for line in devfile:
-        if not any(bad_sen in line for bad_sen in badsentencelist):
+        if str(line[0:5]) != "index" != -1:
             outputfile.write(line)
+        #if all(bad_sen not in line for bad_sen in badsentencelist):
+         #   outputfile.write(line)
+
+#outputfile.close()
 
 #Add mistakeouptuts from wrong_devs to predictions.txt
 with open('predictions_dev.txt') as predsin, open('predictions_dev2.txt', 'w') as predsout:
     line_counter = 1
+    previous_line = -1
     for line in predsin:
-        if str(line_counter) in badindexlist:
-            predsout.write("-1\n")
-            
-        predsout.write(line)
-        line_counter = line_counter + 1
 
-#TODO: we now fixed the problem with wrong_devs
-#TODO: we also have to fix the problems with the other part of wrong_devs sentences
-#THAT IS: Using the wrong_devs index and the above loop, remove any part of the sentence predictions
-#THIS should result in a prediction table and a output.tsv of equal size
+        #remove fragment part 
+        pred = line.replace("dev-COV_fragment01 ","")
 
-#TODO using the above combine output.tsv and the two prediction sets
-#TODO add the current word calculated based upon the index and sentence
+        #find first space
+        space = pred.find(" ")
 
-i=1
+        #index of prediction
+        index = pred[:space]
+
+        if previous_line != -1 and (previous_line+1) != int(index):
+            print(index)
+            predsout.write(f"dev-COV_fragment01 {int(index)-1} 0" + ",-1\n")
+
+        #only write pred if not equal to index
+        if index in badindexlist:
+            predsout.write(f"dev-COV_fragment01 {index} 0" + ",-1\n")
+
+        else:
+            predsout.write(line)
+            line_counter = line_counter + 1
+
+
+        previous_line = int(index)
+
+with open('output.tsv', 'w') as file3:
+    with open('dev2.tsv', 'r') as file1:
+        with open('predictions_dev2.txt', 'r') as file2:
+            for line1, line2 in zip(file1, file2):
+                #line1 = line.strip
+                line2b = line2.strip().replace("dev-COV_fragment01","")
+                komma = line2b.find(",")
+                print(line1.strip(), ",", line2.strip().replace("dev-COV_fragment01 ","")[komma:], file=file3)
+
+#TODO: convert to csv file
+#TODO: add coloring to csv file
+
 #prediction line = dev line + 1
 #
