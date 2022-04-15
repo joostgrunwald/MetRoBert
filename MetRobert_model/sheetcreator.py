bed7357bd83c3491cacad32ef6b8b0abc1273b6d
Joost Grunwald <joostgrunwald2001@gmail.com>
2022-04-15 16:29:02 +0200

    we now create nice word files

--- MetRobert_model/sheetcreator.py
+++ MetRobert_model/sheetcreator.py
@@ -3,23 +3,27 @@
 # from pprint import pprint
 
 import os
-
-# scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
-#          "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
-
-# creds = ServiceAccountCredentials.from_json_keyfile_name(
-#     "credentials.json", scope)
-
-# client = gspread.authorize(creds)
-
-# out = client.create('MetRubert output')
-
-# out.share('commercialtwots@gmail.com', perm_type='user', role='writer')
+from docx.enum.text import WD_COLOR_INDEX
 
 # get current location folder
 location = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
+###############
+# WORD OUTPUT #
+###############
+
+from docx import Document
+from docx.shared import Inches
+
+document = Document()
+
+document.add_heading('MetRuBert Output' , 0)
+
+######################
+# SENTENCE RETRIEVAL #
+######################
+                     
 previous_sentence = None
 pos_list = []
 met_list = []
@@ -36,11 +40,31 @@ with open(os.path.join(location, 'output.tsv'), mode='r', encoding='utf-8') as o
             if sentence != previous_sentence != None:
 
                 #TODO: wrapup
-                # print(previous_sentence)
-                # print(met_list)
-                # print(pos_list)
+                #print(previous_sentence)
+                #print(met_list)
+                #print(pos_list)
 
-                sentences.append(sentence)
+                split_sen = previous_sentence.split()
+                p = document.add_paragraph('')
+                     
+                for i in range(len(split_sen)):
+                     word = split_sen[i]
+                     if str(i) + ' ' in met_list:
+                         #print("reached")
+                         p.add_run(word).bold = True
+                     else:
+                           for pos in pos_list:
+                               #print(pos)
+                               if pos[0] == str(i) + ' ':
+                                   if pos[1] == "NOUN":
+                                       font = p.add_run(word).font
+                                       font.highlight_color = WD_COLOR_INDEX.YELLOW
+                                   else:
+                                       p.add_run(word)
+                               else:
+                                   p.add_run(word)
+                     p.add_run(' ')
+                sentences.append(previous_sentence)
 
                 pos_list = []
                 met_list = []
@@ -49,7 +73,7 @@ with open(os.path.join(location, 'output.tsv'), mode='r', encoding='utf-8') as o
                 # append pos tag
                 pos = splitted[2]
                 index = splitted[3]
-                pos_list.append((index, pos))
+                pos_list.append([index, pos])
 
                 # append metaphor
                 met = splitted[5]
@@ -58,61 +82,71 @@ with open(os.path.join(location, 'output.tsv'), mode='r', encoding='utf-8') as o
 
             previous_sentence = sentence
 
+    
+
+p = document.add_paragraph('A plain paragraph having some ')
+p.add_run('bold').bold = True
+p.add_run(' and some ')
+p.add_run('italic.').italic = True
+
+document.add_page_break()
+
+document.save('MetRubert_runx.docx')
 
 #####################
 # CREATE EXCEL FILE #
 #####################
 
-# import xlsxwriter module
-import xlsxwriter
+# # import xlsxwriter module
+# import xlsxwriter
  
-# Workbook() takes one, non-optional, argument
-# which is the filename that we want to create.
-workbook = xlsxwriter.Workbook('MetRuBert_run_x.xlsx')
+# # Workbook() takes one, non-optional, argument
+# # which is the filename that we want to create.
+# workbook = xlsxwriter.Workbook('MetRuBert_run_x.xlsx')
  
-# The workbook object is then used to add new
-# worksheet via the add_worksheet() method.
-worksheet = workbook.add_worksheet()
+# # The workbook object is then used to add new
+# # worksheet via the add_worksheet() method.
+# worksheet = workbook.add_worksheet()
  
-#HEADER
-worksheet.write("A1", "index")
-worksheet.write("B1", "sentence")
-worksheet.write("C1", "confidence list")
+# #HEADER
+# worksheet.write("A1", "index")
+# worksheet.write("B1", "sentence")
+# worksheet.write("C1", "confidence list")
 
-for i in range(len(sentences)):
-    indexcell = 'A' + str(i+2)
-    sentencecell = 'B' + str(i+2)
+# for i in range(len(sentences)):
+#     indexcell = 'A' + str(i+2)
+#     sentencecell = 'B' + str(i+2)
     
-    worksheet.write(indexcell, i+1)
-    worksheet.write(sentencecell, sentences[i])
+#     worksheet.write(indexcell, i+1)
+#     worksheet.write(sentencecell, sentences[i])
  
-# Finally, close the Excel file
-# via the close() method.
-workbook.close()
+# # Finally, close the Excel file
+# # via the close() method.
+# workbook.close()
 
-#######################
-# FURTHER ADJUST FILE #
-#######################
-import openpyxl
+# #######################
+# # FURTHER ADJUST FILE #
+# #######################
+# import openpyxl
  
-# Give the location of the file
-path = os.path.join(location, "MetRuBert_run_x.xlsx")
+# # Give the location of the file
+# path = os.path.join(location, "MetRuBert_run_x.xlsx")
  
-# To open the workbook
-# workbook object is created
-wb_obj = openpyxl.load_workbook(path)
+# # To open the workbook
+# # workbook object is created
+# wb_obj = openpyxl.load_workbook(path)
  
-# Get workbook active sheet object
-# from the active attribute
-sheet_obj = wb_obj.active
+# # Get workbook active sheet object
+# # from the active attribute
+# sheet_obj = wb_obj.active
 
-#set title
-sheet_obj.title = "MetRubert_sheet"
+# #set title
+# sheet_obj.title = "MetRubert_sheet"
 
-#make column b wider
-sheet_obj.column_dimensions['B'].width = 60
+# #make column b wider
+# sheet_obj.column_dimensions['B'].width = 180
 
-wb_obj.save("MetRuBert_run_x.xlsx")
+# wb_obj.save("MetRuBert_run_x.xlsx")
 
 #! BELOW IS OUTDATED GOOGLE SHEETS CODE, WE USE EXCEL INSTEAD
 # create worksheet
